3faa229db18d6fe72ea5a9afe2a53cdd3af0667c
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-08-26 05:29:59 +0000

    hello code changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout({
     <html lang="en" className={cn("dark", inter.variable, kalam.variable)}>
       <head>
         <title>Edengram</title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2882939249270622"
+             crossorigin="anonymous"></script>
         <meta name="description" content="Create and share interactive emojis." />
         <meta name="google-adsense-account" content="ca-pub-2882939249270622" />
       </head>
