c65b4d611dfd3f9309b66bcf2b5ae2489a33b994
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-05 09:10:52 +0000

    Write full cancellation and refund policy , and 2nd thing is not opening

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,7 +60,7 @@ export async function middleware(request: NextRequest) {
   } = await supabase.auth.getUser()
 
   const url = request.nextUrl
-  const publicPaths = ['/', '/auth/callback', '/terms', '/about', '/privacy', '/blogs', '/contact'];
+  const publicPaths = ['/', '/auth/callback', '/terms', '/about', '/privacy', '/blogs', '/contact', '/cancellation-policy'];
   const isPublicPath = publicPaths.includes(url.pathname);
 
   // Redirect to login if user is not signed in and trying to access a protected route
