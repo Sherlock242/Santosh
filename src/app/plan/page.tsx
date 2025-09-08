6615a2c7b441a50da286f6b9eb136b2bbc4b416b
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-08 01:13:37 +0000

    Can you add upi options in checkout

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -47,6 +47,40 @@ export default function PlanPage() {
             name: "Edengram Gold",
             description: "Monthly Subscription",
             image: "/icon.png",
+            config: {
+              display: {
+                blocks: {
+                  upi: {
+                    name: "Pay with UPI",
+                    instruments: [
+                      { method: "upi" },
+                    ],
+                  },
+                  wallets: {
+                    name: "Pay with Wallets",
+                    instruments: [
+                      { method: "wallet" },
+                    ],
+                  },
+                  netbanking: {
+                    name: "Pay with Netbanking",
+                    instruments: [
+                      { method: "netbanking" },
+                    ],
+                  },
+                  card: {
+                    name: "Pay with Card",
+                    instruments: [
+                      { method: "card" },
+                    ],
+                  },
+                },
+                sequence: ["block.upi", "block.card", "block.netbanking", "block.wallets"],
+                preferences: {
+                  show_default_blocks: true,
+                },
+              },
+            },
             handler: async function (response: any) {
                 // --- Start of new logic ---
                 // Update user to Gold Member immediately on the client-side
