8b3e9b8ad2b351d1ed82c65a68c5d3c4214c6535
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-17 17:59:03 +0000

    This showing

--- src/components/post-view.tsx
+++ src/components/post-view.tsx
@@ -16,6 +16,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import {
