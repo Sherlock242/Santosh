87306746a15abcc0c8f3f11dec7243891d6c8af5
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-09-18 04:14:51 +0000

    I want to confirm before delete , are you using database function get_no

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,4 +8,3 @@ export * from './actions/feed.actions';
 export * from './actions/order.actions';
 export * from './actions/link.actions';
 export * from './actions/payment.actions';
-export * from './actions/post.actions';
